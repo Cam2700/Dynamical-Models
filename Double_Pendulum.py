1c21abf0a10b4e09698efd7ca433641357747302
Cam2700 <bignocam@icloud.com>
2022-06-29 16:37:36 +0100

    tidy code

--- Double_Pendulum.py
+++ Double_Pendulum.py
@@ -7,8 +7,6 @@ import math
 
 def next_state(state, h):
 
-    print("STATE_____________________")
-    print(state)
     temp_state = State([0,0],[0,0],[0,0],[0,0])
 
     k1 = diff_value(state)
@@ -18,143 +16,118 @@ def next_state(state, h):
 
     delta = (k1 + k2*2 + k3*2 + k4)*(h/6)
 
-    print("delta_____________________")
-    print(delta)
-    print(f"{delta.pos_x = }")
-    print(f"{delta.pos_y = }")
-
     temp_state = state + delta
 
-    x1 = 5*math.sin(temp_state.theta[0])### 5 = l1
+    x1 = 5*math.sin(temp_state.theta[0])
     y1 = -5*math.cos(temp_state.theta[0])
 
-    temp_state.pos_x = [x1, x1 + 7*math.sin(temp_state.theta[1])]#### 7 = l2
+    temp_state.pos_x = [x1, x1 + 7*math.sin(temp_state.theta[1])]
     temp_state.pos_y = [y1, y1 - 7*math.cos(temp_state.theta[1])]
-
-    print("STATE2_____________________")
-    print(state)
 
     return temp_state
 
 
 def diff_value(state):
-    
-    print("STATEB_____________________")
-    print(state)
 
     temp_state = State([0,0],[0,0],[0,0],[0,0])
     
-    theta1, theta2 = state.theta[0], state.theta[1]
-    w1, w2 = state.w[0], state.w[1]
+    t1, t2 = state.theta[0], state.theta[1]
+    o1, o2 = state.omega[0], state.omega[1]
 
     m1, m2 = 1, 1.5
     l1, l2 = 5, 7
 
     g = 9.807
 
-    a = -g*(2*m1 + m2)*math.sin(theta1) - m2*g*math.sin(theta1 - 2*theta2) - 2*math.sin(theta1 - theta2)*m2*((w2**2)*l2 + (w1**2)*l1*math.cos(theta1 - theta2))
-    b = l1*(2*m1 + m2 - m2*math.cos(2*theta1 - 2*theta2))
+    a = -g*(2*m1 + m2)*math.sin(t1) - m2*g*math.sin(t1 - 2*t2) - 2*math.sin(t1 - t2)*m2*((o2**2)*l2 + (o1**2)*l1*math.cos(t1 - t2))
+    b = l1*(2*m1 + m2 - m2*math.cos(2*t1 - 2*t2))
 
-    print(f"1{a = }")
-    print(f"1{b = }")
+    temp_state.omega[0] = (a/b)
+    temp_state.theta[0] = state.omega[0]
 
-    temp_state.w[0] = (a/b)
-    temp_state.theta[0] = state.w[0]
+    a = 2*math.sin(t1 - t2)*((o1**2)*l1*(m1 + m2) + g*(m1 + m2)*math.cos(t1) + (o2**2)*l2*m2*math.cos(t1 - t2))
+    b = l2*(2*m1 + m2 - m2*math.cos(2*t1 - 2*t2))
 
-    a = 2*math.sin(theta1 - theta2)*((w1**2)*l1*(m1 + m2) + g*(m1 + m2)*math.cos(theta1) + (w2**2)*l2*m2*math.cos(theta1 - theta2))
-    b = l2*(2*m1 + m2 - m2*math.cos(2*theta1 - 2*theta2))
-
-    print(f"{a = }")
-    print(f"{b = }")
-
-    temp_state.w[1] = (a/b)
-    temp_state.theta[1] = state.w[1]
-
-    # x1 = state.w[0]*l1*math.cos(theta1)
-    # y1 = state.w[0]*l1*math.sin(theta1)
-
-    # temp_state.pos_x = [x1, x1 + state.w[1]*l2*math.cos(theta2)]
-    # temp_state.pos_y = [y1, y1 + state.w[1]*l2*math.sin(theta2)]
-  
-
-    print("STATE2B_____________________")
-    print(state)
-
+    temp_state.omega[1] = (a/b)
+    temp_state.theta[1] = state.omega[1]
 
     return temp_state
 
 
-def update(num, line1, line2, line3, line4, x, y):
+def update(num, dot1, dot2, line3, line4, x, y):
 
     ax.set_title(num)
 
-    line1.set_data(x[num, 0], y[num, 0])
-    line2.set_data(x[num, 1], y[num, 1])
+    dot1.set_data(x[num, 0], y[num, 0])
+    dot2.set_data(x[num, 1], y[num, 1])
 
     line3.set_data([0, x[num, 0]], [0, y[num, 0]])
     line4.set_data([x[num, 0], x[num, 1]], [y[num, 0], y[num, 1]])
 
-    return line1, line2
-
 
 class State():
+
     def __init__(
         self,
-        pos_x = [math.sin((180/math.pi)*25)*5, math.sin((180/math.pi)*25)*5 + 7*math.sin((180/math.pi)*10)],
-        pos_y = [-math.cos((180/math.pi)*25)*5, -math.cos((180/math.pi)*25)*5 - 7*math.cos((180/math.pi)*10)],
-        theta = [(180/math.pi)*25, (180/math.pi)*-10],# 0 = South
-        w = [0, 0]
+        pos_x = [math.sin((180/math.pi)*90)*5, math.sin((180/math.pi)*90)*5 + 7*math.sin((180/math.pi)*-90)],
+        pos_y = [-math.cos((180/math.pi)*90)*5, -math.cos((180/math.pi)*90)*5 - 7*math.cos((180/math.pi)*-90)],
+        theta = [(180/math.pi)*90, (180/math.pi)*-90],# 0 = South
+        omega = [0, 0]
     ):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.theta = theta
-        self.w = w
+        self.omega = omega
 
     def __str__(self):
+
         return "({0},{1})".format(self.pos_x, self.pos_y)
 
     def __add__(self, other):
+
         t_x = [0,0]
         t_y = [0,0]
         t_t = [0,0]
-        t_w = [0,0]
+        t_o = [0,0]
+
         for i in range(2):
             t_x[i] = self.pos_x[i] + other.pos_x[i]
             t_y[i] = self.pos_y[i] + other.pos_y[i]
             t_t[i] = self.theta[i] + other.theta[i]
-            t_w[i] = self.w[i] + other.w[i]
+            t_o[i] = self.omega[i] + other.omega[i]
 
-        return State(t_x, t_y, t_t, t_w)
+        return State(t_x, t_y, t_t, t_o)
 
     def __mul__(self, other):
+
         t_x = [0,0]
         t_y = [0,0]
         t_t = [0,0]
-        t_w = [0,0]
+        t_o = [0,0]
+
         for i in range(2):
             t_x[i] = self.pos_x[i] * other
             t_y[i] = self.pos_y[i] * other
             t_t[i] = self.theta[i] * other
-            t_w[i] = self.w[i] * other
+            t_o[i] = self.omega[i] * other
 
-        return State(t_x, t_y, t_t, t_w)
+        return State(t_x, t_y, t_t, t_o)
     
-size = 10000
-skip = 10
 
-x_array = np.zeros(shape = (size, 2))
-y_array = np.zeros(shape = (size, 2))
+
+size = 1000
+skip = 100
+
+
+
+x_matrix = np.zeros(shape = (size, 2))
+y_matrix = np.zeros(shape = (size, 2))
 
 state = State()
 
 for i in range(skip*size):
     if i%skip == 0:
-        x_array[i//skip, :], y_array[i//skip, :] = state.pos_x, state.pos_y
-
-
-    # state = state + (diff_value(state)*0.01)
-    # print("ADDED_STATE_____________________")
-    # print(state)
+        x_matrix[i//skip, :], y_matrix[i//skip, :] = state.pos_x, state.pos_y
 
     state = next_state(state, 0.0001)
 
@@ -162,21 +135,24 @@ for i in range(skip*size):
 fig = plt.figure()
 ax = fig.add_subplot()
 
-x = x_array
-y = y_array
+x = x_matrix
+y = y_matrix
 
 print(f'{y = }')
 print(f'{x = }')
 
-dot = ax.plot(0, 0, 'o', color = "#1b5e20")# Lines?
+dot = ax.plot(0, 0, 'o', color = "#1b5e20")
 
-line1, = ax.plot(x[:, 0], y[:, 0], 'o', color = "#b71c1c")
-line2, = ax.plot(x[:, 1], y[:, 1], 'o', color = "#311b92")
+dot1, = ax.plot(x[:, 0], y[:, 0], 'o', color = "#b71c1c")
+dot2, = ax.plot(x[:, 1], y[:, 1], 'o', color = "#311b92")
 
 line3, = ax.plot([0, x[0, 1]], [0, y[0, 1]], '-')
 line4, = ax.plot([x[0, 0], x[0, 1]], [y[0, 0], y[0, 1]], '-')
 
 
-ani = animation.FuncAnimation(fig, update, size, fargs=(line1, line2, line3, line4, x, y), interval = 1)
+ani = animation.FuncAnimation(fig, update, size, fargs=(dot1, dot2, line3, line4, x, y), interval = 1)
 
 plt.show()
+
+
+# goodluck future self
